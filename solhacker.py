f3405c2e538cf8080c367970914be97e194c3098
Lesmana Zimmer <lesmana@gmx.de>
2017-01-21 00:30:58 +0100

    merge functions

--- solhacker.py
+++ solhacker.py
@@ -8,38 +8,15 @@ import sys
 from pyamf import sol
 
 # ------------------------------------------------------------------------------
-def sol2py(filename):
-  lines = "".join(sys.stdin.readlines())
-  solobject = sol.decode(lines)
-  pprint.pprint(solobject)
-
-# ------------------------------------------------------------------------------
-def py2sol(filename):
-  lines = "".join(sys.stdin.readlines())
-  pyobject = ast.literal_eval(lines)
-  solobject = sol.encode(pyobject[0], pyobject[1])
-  sys.stdout.write(solobject.getvalue())
-
-# ------------------------------------------------------------------------------
 def main():
-
-  filename = None
+  lines = "".join(sys.stdin.readlines())
   try:
-    filename = sys.argv[1]
-  except:
-    sys.stdout.write("needs filename as argument\n")
-    sys.exit(2)
-
-  filename = os.path.abspath(os.path.expanduser(filename))
-  extension = filename.rpartition(".")[2]
-
-  if extension == "sol":
-    sol2py(filename)
-  elif extension == "py":
-    py2sol(filename)
-  else:
-    sys.stdout.write("extension must be .sol or .py\n")
-    sys.exit(2)
+    solobject = sol.decode(lines)
+    pprint.pprint(solobject)
+  except Exception:
+    pyobject = ast.literal_eval(lines)
+    solobject = sol.encode(pyobject[0], pyobject[1])
+    sys.stdout.write(solobject.getvalue())
 
 # ------------------------------------------------------------------------------
 if __name__ == "__main__":
--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 echo "(u'foo', {u'bar': u'baz'})" > delme.py
 
-./solhacker.sh delme.py < delme.py > delme.sol
+./solhacker.sh < delme.py > delme.sol
 
-./solhacker.sh delme.sol < delme.sol > delme2.py
+./solhacker.sh < delme.sol > delme2.py
 
 diff delme.py delme2.py
