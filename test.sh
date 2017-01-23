520b1a71e5ed5a9b9ee1e1deef8ba9a263745bb5
Lesmana Zimmer <lesmana@gmx.de>
2017-01-23 19:12:22 +0100

    run tests in testdir

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+mkdir -p testdir
+cd testdir
+
 echo "test decode"
-./solhacker.py <<< "solbinaryblob" > delme-decode-actual.txt
+../solhacker.py <<< "solbinaryblob" > delme-decode-actual.txt
 echo "'fakepyamfdecode:solbinaryblob\n'" > delme-decode-expected.txt
 diff delme-decode-actual.txt delme-decode-expected.txt
 
 echo "test encode"
-./solhacker.py <<< "('py', 'tuple')" > delme-encode-actual.txt
+../solhacker.py <<< "('py', 'tuple')" > delme-encode-actual.txt
 echo "fakepyamfencode:pytuple" > delme-encode-expected.txt
 diff delme-encode-actual.txt delme-encode-expected.txt
 
