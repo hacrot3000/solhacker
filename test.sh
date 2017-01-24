393818268ae7c0f979f04ad546702fd3ebbd37ac
Lesmana Zimmer <lesmana@gmx.de>
2017-01-24 09:31:37 +0100

    rename to python repr

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ echo "'fakepyamfdecode:solbinaryblob\n'" > decode-expected.txt
 diff decode-actual.txt decode-expected.txt
 
 echo "test encode"
-../solhacker.py <<< "('py', 'tuple')" > encode-actual.txt
-echo "fakepyamfencode:pytuple" > encode-expected.txt
+../solhacker.py <<< "('python', 'repr')" > encode-actual.txt
+echo "fakepyamfencode:pythonrepr" > encode-expected.txt
 diff encode-actual.txt encode-expected.txt
 
