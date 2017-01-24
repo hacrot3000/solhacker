4f8c6fefbb884622d320174f67ad69a49efe69af
Lesmana Zimmer <lesmana@gmx.de>
2017-01-24 10:16:04 +0100

    rename to solhacker

--- test.sh
+++ test.sh
@@ -8,12 +8,12 @@ cd testdir
 export PYTHONPATH="$PWD:$PYTHONPATH"
 
 echo "test decode"
-../solhacker.py <<< "solbinaryblob" > decode-actual.txt
+../solhacker <<< "solbinaryblob" > decode-actual.txt
 echo "'fakepyamfdecode:solbinaryblob\n'" > decode-expected.txt
 diff decode-actual.txt decode-expected.txt
 
 echo "test encode"
-../solhacker.py <<< "('python', 'repr')" > encode-actual.txt
+../solhacker <<< "('python', 'repr')" > encode-actual.txt
 echo "fakepyamfencode:pythonrepr" > encode-expected.txt
 diff encode-actual.txt encode-expected.txt
 
