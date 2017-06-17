e91ac3b3b9f0725ed64fa5bce181265231794650
Lesmana Zimmer <lesmana@gmx.de>
2017-06-17 20:27:04 +0200

    add error message

--- test.sh
+++ test.sh
@@ -8,12 +8,17 @@ cd testdir
 export PYTHONPATH="$PWD:$PYTHONPATH"
 
 echo "test decode"
-../solhacker <<< "solbinaryblob" > decode-actual.txt
+../solhacker <<< "solbinaryblob" > decode-actual.txt 2>&1
 echo "('string', {'dict': 'solbinaryblob\n'})" > decode-expected.txt
 diff decode-actual.txt decode-expected.txt
 
 echo "test encode"
-../solhacker <<< "('string', {'dict': 'wat'})" > encode-actual.txt
+../solhacker <<< "('string', {'dict': 'wat'})" > encode-actual.txt 2>&1
 echo "solobject:string{'dict': 'wat'}" > encode-expected.txt
 diff encode-actual.txt encode-expected.txt
+
+echo "test error"
+../solhacker <<< "syntax error" > error-actual.txt 2>&1
+echo "error: cannot decode nor encode" > error-expected.txt
+diff error-actual.txt error-expected.txt
 
